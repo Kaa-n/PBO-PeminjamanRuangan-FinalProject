3c06500ebf80945e26ff9a7a20138711e6464974
Kaa-n <tanjakanijo@gmail.com>
2026-01-04 23:35:14 +0800

    connector setting

--- pbopeminjamanruanganfx/src/main/java/module-info.java
+++ pbopeminjamanruanganfx/src/main/java/module-info.java
@@ -1,6 +1,7 @@
 module org.pbopeminjamanruanganjavafx {
     requires javafx.controls;
     requires javafx.fxml;
+    requires java.sql;
 
     opens org.pbopeminjamanruanganjavafx to javafx.fxml;
     exports org.pbopeminjamanruanganjavafx;
