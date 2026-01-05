399db095ad2db80c656b37eb959d5ccd6c212344
gunammr <gunajanuarta6@gmail.com>
2026-01-05 20:47:34 +0800

    Menyambungkan controller navigasi di admin detail peminjaman

---
+++ pbopeminjamanruanganfx/src/main/java/org/pbopeminjamanruanganjavafx/controller/KelolaUserController.java
@@ -0,0 +1,10 @@
+package org.pbopeminjamanruanganjavafx.controller;
+
+import java.io.IOException;
+import org.pbopeminjamanruanganjavafx.App;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+
+public class KelolaUserController {
+
+}
